3762faa3d2ee7a12ad06d278602c06077b052fcc
Eitan02 <eitan.derdiger@gmail.com>
2025-05-30 19:31:32 +0300

    final changes

--- tests/test_general.cpp
+++ tests/test_general.cpp
@@ -1,4 +1,4 @@
-// eitan.derdiger@gmail.com
+//eitan.derdiger@gmail.com
 #include "doctest.h"
 #include "../roles/General.hpp"
 #include "../game/Player.hpp"
--- tests/test_judge.cpp
+++ tests/test_judge.cpp
@@ -1,4 +1,4 @@
-// eitan.derdiger@gmail.com
+//eitan.derdiger@gmail.com
 #include "doctest.h"
 #include "../roles/Judge.hpp"
 #include "../roles/Baron.hpp"
